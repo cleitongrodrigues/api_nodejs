e7e7de4760a37e123970ae3581b92e6bdce371a9
CLEITON GUILHERME RODRIGUES DE SOUZA <cleiton.souza50>
2024-04-04 20:47:21 -0300

    Aula 04-04-2024

--- controllers/produtos.js
+++ controllers/produtos.js
@@ -4,21 +4,47 @@ const db = require('../database/connection');
 module.exports = {
     async listarProdutos(request, response) {
         try {
+            const sql=`select 
+            prd_id, prd_nome, prd_valor, prd_unidade, ptp_id,prd_disponivel, 
+            prd_img, prd_destaque, prd_img_destaque, prd_descricao 
+            from produtos where prd_disponivel=1;`
+            // executa instruções SQL e armazena o resultado na variável usuários
+            const produtos = await db.query(sql);
+            // armazena em uma variável o número de registros retornados
+            const nItens = produtos[0].length;
+
             return response.status(200).json({
                 sucesso: true, 
                 mensagem: 'Lista de produtos.', 
-                dados: null
+                dados: produtos[0],
+                nItens
+                
             });
         } catch (error) {
             return response.status(500).json({
                 sucesso: false, 
-                mensagem: `Erro na requisição. \n ${error}`, 
+                mensagem: `Erro na requisição. -${error}`, 
                 dados: null
             });
         }
-    },
+    }, 
     async cadastrarProdutos(request, response) {
         try {
+                // parâmetros recebidos no corpo da requisição
+                const { prd_nome, prd_valor, prd_unidade, ptp_id,prd_disponivel, 
+                    prd_img, prd_destaque, prd_img_destaque, prd_descricao } = request.body;
+                // instrução SQL
+                const sql = `INSERT INTO produtos 
+                    (prd_nome, prd_valor, prd_unidade, ptp_id,prd_disponivel, 
+                        prd_img, prd_destaque, prd_img_destaque, prd_descricao ) 
+                    VALUES (?, ?, ?, ?, ?, ?)`;
+                // definição dos dados a serem inseridos em um array
+                const values = [prd_nome, prd_valor, prd_unidade, ptp_id,prd_disponivel, 
+                    prd_img, prd_destaque, prd_img_destaque, prd_descricao ];
+                // execução da instrução sql passando os parâmetros
+                const execSql = await db.query(sql, values);
+                // identificação do ID do registro inserido
+                const usu_id = execSql[0].insertId;
             return response.status(200).json({
                 sucesso: true, 
                 mensagem: 'Cadastro de produtos.', 
@@ -27,11 +53,11 @@ module.exports = {
         } catch (error) {
             return response.status(500).json({
                 sucesso: false, 
-                mensagem: `Erro na requisição. \n ${error}`, 
+                mensagem: `Erro na requisição. -${error}`, 
                 dados: null
             });
         }
-    },
+    }, 
     async editarProdutos(request, response) {
         try {
             return response.status(200).json({
@@ -42,11 +68,11 @@ module.exports = {
         } catch (error) {
             return response.status(500).json({
                 sucesso: false, 
-                mensagem: `Erro na requisição. \n ${error}`, 
+                mensagem: `Erro na requisição. -${error}`, 
                 dados: null
             });
         }
-    },
+    }, 
     async apagarProdutos(request, response) {
         try {
             return response.status(200).json({
@@ -57,10 +83,10 @@ module.exports = {
         } catch (error) {
             return response.status(500).json({
                 sucesso: false, 
-                mensagem: `Erro na requisição. \n ${error}`, 
+                mensagem: `Erro na requisição. -${error}`, 
                 dados: null
             });
         }
-    },
+    }, 
 }
 
